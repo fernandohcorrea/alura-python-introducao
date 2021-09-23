5a88b90c132cda57e37bf4c9219df6060b474159
Fernando H Correa <fernando.correa@kymo.it>
2021-09-23 09:44:27 -0300

    20210923 - Comparação de Tipos IF Else

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -1,1 +1,18 @@
+print("*********************************")
 print("Bem vindo ao jogo de adivinhação")
+print("*********************************")
+
+numero_secreto = 55
+
+chute = input("Digite um número: ")
+chute = int(chute)
+
+print("Você digito",":", chute)
+
+if( chute == numero_secreto ):
+    print("Você acertou")
+else:
+    print("Você errou")
+
+
+print("Fim do Jogo")
