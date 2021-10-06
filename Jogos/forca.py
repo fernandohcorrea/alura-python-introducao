74d73c8759cdabaab45e950626f1804c0e7225a4
Fernando H Corrêa <fernandohcorrea@gmail.com>
2021-10-06 10:05:05 -0300

    20211006 - Iniciando implementção do jogo da Forca

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -4,6 +4,21 @@ def jogar():
     print("Bem vindo ao jogo de forca")
     print("*********************************")
 
+    palavra_secreta = "queijo"
+
+    enforcado = False
+    acertou = False
+    chute = ""
+
+    while ( not enforcado and not acertou ):
+        print("*********************************")
+        chute = input("Digite uma letra: ")
+
+        if (chute == 'q') :
+            acertou = True
+
+
+
 
     print("\n")
     print("*********************************")
